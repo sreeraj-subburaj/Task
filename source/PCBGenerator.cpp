2d11594dab1a5a2d353176fe8a328c5a8ee58912
sreeraj-subburaj <147267238+sreeraj-subburaj@users.noreply.github.com>
2023-11-05 22:30:53 -0500

    Update PCBGenerator.cpp

--- source/PCBGenerator.cpp
+++ source/PCBGenerator.cpp
@@ -47,11 +47,10 @@ void PCBGenerator::readnext(){
 
         ss << line;
         int count = 0;
-        while(count < 4 && ss >> vals[count]){
+        while(count < 5 && ss >> vals[count]){
             count++;
         };
         while(vals[0] >= arr_size || vals[1] >= arr_size) doublearrays();
-
         //series of error checking and data validation, the if(error = true) just compacts code
         if(ss.fail() && !error) if(error = true) std::cout << "Missing data for process in file. Exiting Now." << std::endl;
         if(ss >> vals[5] && !error) if(error = true) std::cout << "Too many values for a process in file. Exiting now." << std::endl;
@@ -59,11 +58,10 @@ void PCBGenerator::readnext(){
         if(vals[2] <= 0 && !error) if(error = true) std::cout << "CPU Burst time must be greater than 0. Exiting now." << std::endl;
         if(vals[1] < last_arr && !error) if(error = true) std::cout << "File needs to be sorted by arrival time. Exiting now." << std::endl;
         if(pids[int(vals[0])]) if(error = true) std::cout << "Can't have duplicate PIDs. Exiting now." << std::endl;
-        if(arrivals[int(vals[1])]) if(error = true) std::cout << "Can't have duplicate arrival times. Exiting now." << std::endl;
+        //if(arrivals[int(vals[1])]) if(error = true) std::cout << "Can't have duplicate arrival times. Exiting now." << std::endl;
 
         if(error) return(throw 1);
-
-        //no error with data, continue
+  	
         arrivals[int(vals[1])] = true;
         pids[int(vals[0])] = true;
         nextPCB = PCB(vals[0], vals[1], vals[2], vals[3]);
@@ -76,21 +74,34 @@ bool PCBGenerator::finished(){
 }
 
 void PCBGenerator::doublearrays(){
-    arr_size *= 2;
-    auto temp_arrs = new bool[arr_size];
-    auto temp_pids = new bool[arr_size];
-    for(int i = 0; i < arr_size; ++i) {
-        if(i < arr_size/2){
-            temp_arrs[i] = arrivals[i];
-            temp_pids[i] = pids[i];
-        }
-        else {
-            temp_arrs[i] = false;
-            temp_pids[i] = false;
-        }
+
+  arr_size *= 2;
+  
+  // Create new larger arrays
+  auto temp_arrs = new bool[arr_size]; 
+  auto temp_pids = new bool[arr_size];
+  
+  // Copy values from old arrays
+  for(int i = 0; i < arr_size; ++i) {
+
+    if(i < arr_size/2){
+      temp_arrs[i] = arrivals[i];
+      temp_pids[i] = pids[i];
+
     }
-    delete arrivals;
-    delete pids;
-    arrivals = temp_arrs;
-    pids = temp_pids;
+    else {
+      // Initialize new array values
+      temp_arrs[i] = false; 
+      temp_pids[i] = false;
+    }
+
+  }
+  
+  // Delete old arrays
+  delete arrivals;
+  delete pids;
+  
+  // Update to new arrays
+  arrivals = temp_arrs;
+  pids = temp_pids;
 }
